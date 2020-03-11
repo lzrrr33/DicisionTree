0433b1b8a18b512572274551480115f6b81a834d
lzrrr-rose <49428609+lzrrr33@users.noreply.github.com>
2020-03-11 10:49:03 +0800

    Update DicisionTree.py

--- DicisionTree.py
+++ DicisionTree.py
@@ -1,8 +1,10 @@
 from math import log
 import operator
-import  random
-def chooseLeft(data,labels):
-    featurenum = len(data[0])-1
+import random
+
+
+def chooseLeft(data, labels):
+    featurenum = len(data[0]) - 1
     baseEntropy = computeShannonEnt(data)
     maxGain = 0.0
     bestFeature = -1
@@ -24,7 +26,7 @@ def chooseLeft(data,labels):
         # 信息增益
         infoGain = baseEntropy - newEntropy
         # 打印每个特征的信息增益
-        
+
         print("特征%s的增益为%.3f" % (labels[i], infoGain))
         # 计算信息增益
         if (infoGain > maxGain):
@@ -33,7 +35,7 @@ def chooseLeft(data,labels):
             # 记录信息增益最大的特征的索引值
             bestFeature = i
             # 返回信息增益最大特征的索引值
-    print("最大增益特征：",labels[bestFeature])
+    print("最大增益特征：", labels[bestFeature])
     return bestFeature
 
 
@@ -43,42 +45,44 @@ def getdataset(path):
     data = []
     for line in f.readlines():
         lines = line.split(',')
-        p=lines[1]
-        s=lines[2]
-        a=float(lines[3])
-        Em=lines[4]
-        Sex=lines[5]
-        if p=='"1st"':
+        p = lines[1]
+        s = lines[2]
+        a = float(lines[3])
+        Em = lines[4]
+        Sex = lines[5]
+        if p == '"1st"':
             pclass = 0
-        elif p=='"2nd"':
+        elif p == '"2nd"':
             pclass = 1
         else:
             pclass = 2
-        if s=='0':
+        if s == '0':
             Survived = 0
         else:
             Survived = 1
-        if a<6:
+        if a < 6:
             age = 0
-        elif a<18:
+        elif a < 18:
             age = 1
-        elif a<50:
+        elif a < 50:
             age = 2
         else:
             age = 3
-        if Em=='"Southampton"':
+        if Em == '"Southampton"':
             Embarked = 0
-        elif Em=='"Cherbourg"':
+        elif Em == '"Cherbourg"':
             Embarked = 1
         else:
             Embarked = 2
-        if Sex=='"female"\n':
+        if Sex == '"female"\n':
             sex = 0
         else:
             sex = 1
-        data.append([pclass,age,Embarked,sex,Survived])
-    label = ["pclass","age","Embarked","sex"]
+        data.append([pclass, age, Embarked, sex, Survived])
+    label = ["pclass", "age", "Embarked", "sex"]
     return data, label
+
+
 # compute ShannonEnt
 
 def computeShannonEnt(dataset):
@@ -99,77 +103,82 @@ def computeShannonEnt(dataset):
         shannonEnt -= prob * log(prob, 2)  # 利用公式计算
     return shannonEnt  # 返回经验熵
 
-def splitDataSet(dataSet,axis,value):
-    retDataSet=[]
+
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
     for featVec in dataSet:
-        if featVec[axis]==value:
-            reducedFeatVec=featVec[:axis]
-            reducedFeatVec.extend(featVec[axis+1:])
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis + 1:])
             retDataSet.append(reducedFeatVec)
     return retDataSet
 
+
 def majorityCnt(classList):
-    classCount={}
-    #统计classList中每个元素出现的次数
+    classCount = {}
+    # 统计classList中每个元素出现的次数
     for vote in classList:
         if vote not in classCount.keys():
-            classCount[vote]=0
-        classCount[vote]+=1
-        #根据字典的值降序排列
-        sortedClassCount=sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)
+            classCount[vote] = 0
+        classCount[vote] += 1
+        # 根据字典的值降序排列
+        sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
         return sortedClassCount[0][0]
 
 
-def createTree(dataSet,labels,featLabels):
-    #取分类标签（是否存活：yes or no）
-    classList=[example[-1] for example in dataSet]
-    #如果类别完全相同，则停止继续划分
-    
-    if len(labels)==0:
+def createTree(dataSet, labels, deepth):
+    # 取分类标签（是否存活：yes or no）
+    classList = [example[-1] for example in dataSet]
+    # 如果类别完全相同，则停止继续划分
+    if deepth == 0:
         return max(set(classList), key=classList.count)
-    if classList.count(classList[0])==len(classList):
+    if classList.count(classList[0]) == len(classList):
         return classList[0]
-    #遍历完所有特征时返回出现次数最多的类标签
-    if len(dataSet[0])==1:
+    # 遍历完所有特征时返回出现次数最多的类标签
+    if len(dataSet[0]) == 1:
         # print(majorityCnt(classList))
         return majorityCnt(classList)
-    #选择最优特征
-    bestFeat=chooseLeft(dataSet,labels)
-    #最优特征的标签
+    # 选择最优特征
+    bestFeat = chooseLeft(dataSet, labels)
+    # 最优特征的标签
 
-    bestFeatLabel=labels[bestFeat]
-    featLabels.append(bestFeatLabel)
-    #根据最优特征的标签生成树
-    myTree={bestFeatLabel:{}}
-    #删除已经使用的特征标签
-    del(labels[bestFeat])
-    #得到训练集中所有最优特征的属性值
-    featValues=[example[bestFeat] for example in dataSet]
-    #去掉重复的属性值
-    uniqueVls=set(featValues)
-    #遍历特征，创建决策树
+    bestFeatLabel = labels[bestFeat]
 
+    # 根据最优特征的标签生成树
+    myTree = {bestFeatLabel: {}}
+    # 删除已经使用的特征标签
+    # del (labels[bestFeat])
+    # 得到训练集中所有最优特征的属性值
+    featValues = [example[bestFeat] for example in dataSet]
+    # 去掉重复的属性值
+    uniqueVls = set(featValues)
+    # 遍历特征，创建决策树
+    feaLabel = []
+    for label in labels:
+        if label!=bestFeatLabel:
+            feaLabel.append(label)
     for value in uniqueVls:
-        myTree[bestFeatLabel][value]=createTree(splitDataSet(dataSet,bestFeat,value),
-                                               labels,featLabels)
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value),
+                                                  feaLabel, deepth-1)
     return myTree
 
-def classify(tree, fealabel,people):
+
+def classify(tree, people, labels):
     classLabel = 0
     # 获取决策树节点
     firstStr = next(iter(tree))
     # 下一个字典
     secondDict = tree[firstStr]
+    for k in tree.keys():
+        featIndex = labels.index(k)
+        for key in secondDict.keys():
+            if people[featIndex] == key:
+                if type(secondDict[key]).__name__ == 'dict':
+                    classLabel = classify(secondDict[key], people,labels)
+                else:
+                    classLabel = secondDict[key]
+        return classLabel
 
-    featIndex = fealabel.index(firstStr)
-
-    for key in secondDict.keys():
-        if people[featIndex] == key:
-            if type(secondDict[key]).__name__ == 'dict':
-                classLabel = classify(secondDict[key], fealabel, people)
-            else:
-                classLabel = secondDict[key]
-    return classLabel
 
 def compute(data):
     mannum = 0
@@ -177,16 +186,16 @@ def compute(data):
     womennum = 0
     womenlive = 0
     for p in data:
-        if p[3]==0:
-            womennum+=1
-            if p[4]==1:
-                womenlive+=1
+        if p[3] == 0:
+            womennum += 1
+            if p[4] == 1:
+                womenlive += 1
         else:
-            mannum+=1
-            if p[4]==1:
-                manlive+=1
-    print('男性存活率:',manlive/mannum)
-    print('女性存活率:',womenlive/womennum)
+            mannum += 1
+            if p[4] == 1:
+                manlive += 1
+    print('男性存活率:', manlive / mannum)
+    print('女性存活率:', womenlive / womennum)
 
 
 
@@ -195,14 +204,16 @@ if __name__ == '__main__':
     random.shuffle(data)
     traindata = data[:425]
     testdata = data[425:]
-    featLabels = []
-    myTree = createTree(traindata, labels, featLabels)
+
+    myTree = createTree(traindata, labels, 4)
     rightnum = 0
+    print(labels)
+    print("tree:", myTree)
+
     for people in testdata:
         label = people[4]
-        out = classify(myTree,featLabels,people)
-        if out==label:
-            rightnum+=1
-    print("tree:",myTree)
-    print("ap:",rightnum/len(testdata))
- 
+        out = classify(myTree, people,labels)
+        if out == label:
+            rightnum += 1
+
+    print("ap:", rightnum / len(testdata))
